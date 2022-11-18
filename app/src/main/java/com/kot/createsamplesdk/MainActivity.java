8cf7fa9a2b884d681c42f3d427ef0e15cacb5582
Manoj Sharma <msharma@anystr.com>
2022-11-18 10:57:31 +0530

    2 commit

--- app/src/main/java/com/kot/createsamplesdk/MainActivity.java
+++ app/src/main/java/com/kot/createsamplesdk/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.kot.samplesdk.Dashboard;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivity(new Intent(this, Dashboard.class));
+  
+       // startActivity(new Intent(this, ));
     }
 }
